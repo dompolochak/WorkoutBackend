f332c9d9bd08e20ce48675b26b8437f2aa3cfd88
dompolochak <dp121@zips.uakron.edu>
2020-11-29 20:36:55 -0500

    Hide password

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const doItLive=packageJson.isLive;
 const connection = mysql.createConnection({
     host: 'workoutdb.cqhauiwyqpbl.us-east-2.rds.amazonaws.com',
     user: 'admin',
-    password: 'watermelon',
+    password: process.env.password,
     database: 'workoutdb'
 });
 
